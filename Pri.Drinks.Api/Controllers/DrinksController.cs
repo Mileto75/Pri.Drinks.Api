8b18f6ddac2c9a62e44d7afe10a48fcf0f1a0327
Miledi75 <mileto.di.marco@howest.be>
2023-10-02 16:36:55 +0200

    Add Api project

---
+++ Pri.Drinks.Api/Controllers/DrinksController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Drinks.Core.Interfaces.Services;
+
+namespace Pri.Drinks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrinksController : ControllerBase
+    {
+        private readonly IDrinkService _drinkService;
+
+        public DrinksController(IDrinkService drinkService)
+        {
+            _drinkService = drinkService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+            var drinks = await _drinkService.GetAllAsync();
+            return Ok(drinks);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"drinks with id:{id}");
+        }
+        [HttpGet("{name}")]
+        public IActionResult SearchByName(string name)
+        {
+            return Ok($"You searched drink:{name}");
+        }
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok("Created");
+        }
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok("Updated");
+        }
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Deleted");
+        }
+    }
+}
